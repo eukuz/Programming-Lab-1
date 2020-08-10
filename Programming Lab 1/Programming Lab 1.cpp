f1d42819768288e8994332d6b22f4a56f4d5bea1
evgn.worker <evgn.worker@gmail.com>
2020-08-10 17:07:04 +0500

    1st task completed

---
+++ Programming Lab 1/Programming Lab 1.cpp
@@ -0,0 +1,40 @@
+// Example program
+#include <iostream>
+#include <string>
+
+float lenght(float x1, float y1, float x2, float y2)
+{
+    return sqrt(pow(x1 - x2, 2) + pow(y1 - y2, 2));
+}
+
+bool existanceOfATriangle(float x1, float y1, float x2, float y2, float x3, float y3)
+{
+
+    float lc = lenght(x1, y1, x2, y2);
+    float lb = lenght(x1, y1, x3, y3);
+    float la = lenght(x2, y2, x3, y3);
+    if (lc + lb > la && la + lb > lc && lc + la > lb)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+
+int main()
+{
+    float x1, y1, x2, y2, x3, y3;
+    std::cout << "input x1,y1,x2,y2,x3,y3 separated with space symbols: ";
+    std::cin >> x1 >> y1 >> x2 >> y2 >> x3 >> y3;
+    bool ex = existanceOfATriangle(x1, y1, x2, y2, x3, y3);
+    if (ex) {
+
+        std::cout << "Such a triangle can exist";
+    }
+    else {
+        std::cout << "Such a triangle cannot exist";
+    }
+}
+
